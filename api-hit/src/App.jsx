c69cd7f3275bca73d15f54d0389a4a0f70a3b5f1
PreshaShrestha4 <preshashrestha041@gmail.com>
2025-02-03 10:58:05 +0545

    update

--- api-hit/src/App.jsx
+++ api-hit/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/loginform";
 const App = () => {
   return (
     <div>
       <LoginForm />
+      <RegisterForm />
     </div>
   );
 };
